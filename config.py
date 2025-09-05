9366a1533a14dfc7504d1c9ae15cdaecfc951035
williamnoal <williamnoal@gmail.com>
2025-09-05 14:52:38 -0300

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# config.py
-#
-# IMPORTANTE: Substitua "SUA_CHAVE_DE_API_AQUI" pela sua chave de API real do Google AI Studio.
-# Você pode obter uma chave em: https://aistudio.google.com/app/apikey
-API_KEY = "AIzaSyCm_--Gcj6KzHpJJASp7nosNaBOsUGZvbk"
